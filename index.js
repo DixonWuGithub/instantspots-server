96e7bfc053f4938ad9c28576263989cce72af319
Dixon Wu <dxnwuconnect@gmail.com>
2023-02-14 06:26:50 -0800

    implement data persistance to json file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const WebSocket = require("ws");
 const http = require('http')
-
+const fs = require('node:fs');
+const stringify = require('fast-safe-stringify')
 const app = express();
 const server = http.createServer(app);
 const socketServer = new WebSocket.Server({ server });
@@ -11,7 +12,9 @@ const clientsList = [];
 const commentArray = [];
 let asks = [];
 
+
 socketServer.on('connection', (socket) => {
+  readTopics()
   console.log('Client connected');
   clientsList.push(socket);
 
@@ -29,8 +32,8 @@ socketServer.on('connection', (socket) => {
       case 'sendMessage':
         const sender = data.sender;
         const broadcastMessage = `${sender.name} said: ${data.message}`;
-        commentArray.push(broadcastMessage)
-        console.log(commentArray)
+        // commentArray.push(broadcastMessage)
+        // console.log(commentArray)
         console.log('roomname is ', data.roomName)
         sendMessage(socket, data.roomName, data.sender, broadcastMessage);
         break;
@@ -58,9 +61,14 @@ socketServer.on('connection', (socket) => {
 function leaveCurrentRoom(socket) {
   const ask = asks.find(a => a.clients.includes(socket));
   if (ask) {
-      const clientIndex = ask.clients.findIndex(client => client === socket);
-      ask.clients.splice(clientIndex, 1);
+    const clientIndex = ask.clients.findIndex(client => client === socket);
+    ask.clients.splice(clientIndex, 1);
   }
+
+  socket.send(JSON.stringify({
+    type: 'newMessage',
+    message: []
+  }));
 }
 
 function joinRoom(socket, roomName) {
@@ -68,11 +76,13 @@ function joinRoom(socket, roomName) {
   if (!ask) {
     ask = { name: roomName, clients: [], messages: [] };
     asks.push(ask);
+    
   }
   console.log(asks)
   console.log(ask)
   ask.clients.push(socket);
   socket.ask = ask;
+  writeTopicsToFile(asks)
 
   ask.messages.forEach((message) => {
     socket.send(JSON.stringify({
@@ -85,9 +95,10 @@ function joinRoom(socket, roomName) {
 }
 
 function sendMessage(socket, roomName, sender, message) {
-  const ask = asks.find(a => a.name === roomName);
+  let ask = asks.find(a => a.name === roomName);
   console.log('ask is', ask)
   if (!ask) {
+    console.log("ask not found")
     return;
   }
 
@@ -97,6 +108,7 @@ function sendMessage(socket, roomName, sender, message) {
 
   console.log(`Received message: ${message}`);
   ask.messages.push(message);
+  writeTopicsToFile(asks)
   ask.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify({
@@ -108,6 +120,18 @@ function sendMessage(socket, roomName, sender, message) {
     // const id = uuidv4()
     // const sender = socket._socket.remoteAddress + ':' + socket._socket.remotePort;
   });
+}
+
+function readTopics() {
+  try {
+    asks = JSON.parse(fs.readFileSync('./data/topics/topics.json'));
+  } catch (e) {
+    console.error('Error reading topics:', e);
+  }
+}
+
+function writeTopicsToFile(asks) {
+  fs.writeFileSync('./data/topics/topics.json', stringify(asks));
 }
 
 server.listen(8080, function () {
